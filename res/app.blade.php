a004dac9768d28d9faef7e3de2474e9e7dd28de3
Antonio Ianzano <antonio@ianzano.me>
2024-04-14 23:12:02 +0200

    fix

--- res/app.blade.php
+++ res/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml" href="/assets/diana-MWv7s2gH.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>{{ $app_name }}</title>
+    <title>{{ $name }}</title>
 
     <style>
         .benchmark {
